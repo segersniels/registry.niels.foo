052ef2a46ed4675e838696da1911bf2e5048159a
segersniels <segers.n@hotmail.com>
2024-10-19 14:02:23 +0200

    fix: use existing headers instead of custom middleware header

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,8 +4,16 @@ import { headers } from "next/headers";
 
 import { type Schema } from "@/scripts/build";
 
+async function getOrigin() {
+  const headersList = await headers();
+  const host = headersList.get("X-Forwarded-Host");
+  const proto = headersList.get("X-Forwarded-Proto");
+
+  return `${proto}://${host}`;
+}
+
 export async function getSchema(name: string): Promise<Schema> {
-  let hostUrl = headers().get("x-host-url");
+  let hostUrl = await getOrigin();
   if (!hostUrl) {
     hostUrl = "https://registry.niels.foo";
   }
