1aff8b3f5f780b2f5777dedfe533f88e28f1a3fa
segersniels <segers.n@hotmail.com>
2024-12-23 12:01:06 +0100

    refactor: hide source by default

--- src/components/source.tsx
+++ src/components/source.tsx
@@ -1,8 +1,35 @@
+import { ChevronsUpDown } from "lucide-react";
+
 import { getSchema } from "@/app/actions";
 import Code from "@/components/code";
+
+import { LayoutSectionTitle } from "./layout";
+import { Button } from "./ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
 
 export default async function Source({ name }: { name: string }) {
   const source = await getSchema(name);
 
-  return <Code language="tsx">{source.files[0].content}</Code>;
+  return (
+    <Collapsible>
+      <CollapsibleTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex items-center space-x-2 p-0 hover:bg-inherit"
+        >
+          <LayoutSectionTitle>Source</LayoutSectionTitle>
+          <ChevronsUpDown className="w-4 h-4" />
+        </Button>
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <Code language="tsx" className="my-4">
+          {source.files[0].content}
+        </Code>
+      </CollapsibleContent>
+    </Collapsible>
+  );
 }
